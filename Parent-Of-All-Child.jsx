56723bc16c324814aa82b5ae203216249db659d0
Hanzla Shahzad <hanzlashahzad16@gmail.com>
2025-03-06 14:44:23 +0500

    Add files via upload
    Parent of all childs

---
+++ Parent-Of-All-Child.jsx
@@ -0,0 +1,16 @@
+import Increatment from "./increament";
+import TodoApp from "./showArray";
+import IncreDecrement from "./useStatePr";
+import RegistrationForm from "./registrationForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <>
+      <RegistrationForm></RegistrationForm>
+      {/* <IncreDecrement></IncreDecrement> */}
+      {/* <TodoApp></TodoApp> */}
+      {/* <Increatment></Increatment> */}
+    </>
+  );
+}
